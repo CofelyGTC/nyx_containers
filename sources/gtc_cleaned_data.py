99ecd4b1a802f87f9fdee18272a56cd59ef8669e
debockpierrecofely <pierre.debock@engie.com>
2019-10-10 17:25:52 +0200

    savepoint

--- sources/gtc_cleaned_data.py
+++ sources/gtc_cleaned_data.py
@@ -25,6 +25,9 @@ from datetime import timezone
 from io import StringIO
 from dateutil import tz
 import dateutil.parser
+import pytz
+from dateutil.tz import tzlocal
+from tzlocal import get_localzone
 
 from elastic_helper import es_helper 
 
@@ -33,9 +36,9 @@ from elastic_helper import es_helper
 import tzlocal # $ pip install tzlocal
 
 
-VERSION="1.0.6"
-MODULE="GTC_SITES_DATA"
-QUEUE=["GTC_SITES_DATA_temp1"]
+VERSION="0.0.1"
+MODULE="GTC_CLEANED_DATA"
+QUEUE=["GTC_CLEANED_DATA_RANGE"]
 
 query = {
   "aggs": {
@@ -159,95 +162,58 @@ def getTimestamp(timeD):
 ################################################################################
 def messageReceived(destination,message,headers):
     global es
-    records=0
-    starttime = time.time()
+
     logger.info("==> "*10)
     logger.info("Message Received %s" % destination)
-    logger.info(headers)
 
-    if "CamelFileNameOnly" in headers:
-        headers["file"] = headers["CamelFileNameOnly"]
+    msg = json.loads(message)
+    start = datetime.fromtimestamp(int(msg['start']))
+    stop = datetime.fromtimestamp(int(msg['stop']))
 
-    if "file" in headers:
-        logger.info("File:%s" %headers["file"])
-        log_message("Import of file [%s] started." % headers["file"])
-
-    filename = headers["CamelFileAbsolutePath"]
-    mes=base64.b64decode(message)
-    mes = mes.decode('utf-8')
-    #logger.info("Message [%d]:%s" %(len(mes),mes))
-    if((filename != None) and (len(mes)>10)):
-        try:
-            handleOneMessage(filename,mes)
-            cleanData(filename, mes)
-        except Exception as e:
-            logger.error("Unable to decode message")
-            error = traceback.format_exc()
-            logger.error(error)
-            endtime = time.time()
-            logger.error(e,exc_info=True)
-            log_message("Import of file [%s] failed. Duration: %d Exception: %s." % (headers["file"],(endtime-starttime),str(e)))   
-
-
-    #if len(reserrors)>0:
-    #   log_message("Import of file [%s] failed. Duration: %d. %d records were not imported." % (headers["file"],(endtime-starttime),len(reserrors)))        
-
-    endtime = time.time()    
-    try:
-        log_message("Import of file [%s] finished. Duration: %d Records: %d." % (headers["file"],(endtime-starttime),df.shape[0]))         
-    except:
-        log_message("Import of file [%s] finished. Duration: %d." % (headers["file"],(endtime-starttime)))    
+    while start < stop:
+        df=retrieve_raw_data(start)
+        print(start)
+        for datestr, df_date in df.groupby('date'):
+            tempdf = df_date[['name']]
+            tempdf[1] = df_date[['date']]
+            tempdf[2] = 2
+            tempdf[3] = df_date[['value']]
+            tempdf[4] = 0
+            tempdf = tempdf.reset_index(drop= True)
+            tempdf = tempdf.rename({'name': 0}, axis='columns')
+            datestr = datestr.replace('-','')
+            datestr = datestr.replace(':','')
+            datestr = datestr.replace(' ','_')
+            filename = 'LUTOSA_ExportNyxAWS_'+datestr+'.CSV'
+            try:    
+                cleanData(filename, tempdf)
+            except Exception as er:
+                logger.error('Cannot cleaned data for datetime : '+ datestr)
+                logger.error(er)
+            
+        start = start + timedelta(1)
     
-        
+def retrieve_raw_data(day):
+    start_dt = datetime(day.year, day.month, day.day)
+    end_dt   = datetime(start_dt.year, start_dt.month, start_dt.day, 23, 59, 59)
 
-    logger.info("<== "*10)
+    df_raw=es_helper.elastic_to_dataframe(es, index='opt_sites_data*', 
+                                           query='client: COGLTS', 
+                                           start=start_dt, 
+                                           end=end_dt,
+                                           size=1000000)
 
+    containertimezone=pytz.timezone(get_localzone().zone)
+    df_raw['@timestamp'] = pd.to_datetime(df_raw['@timestamp'], \
+                                               unit='ms', utc=True).dt.tz_convert(containertimezone)
+    df_raw=df_raw.sort_values('@timestamp') 
+    
+    return df_raw
 
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=tz.tzlocal())
 
-def updateCache(meter,meterdate,curvalue):
-    global lastvaluecache
-    #print(meter)
-    key=meter+meterdate.strftime('%Y-%m-%d %H:%M')
-    lastvaluecache[meter]={"key":key,"value":curvalue}
-    if((meterdate.hour==0) and (meterdate.minute==0)):
-        dayvaluecache[meter]={"key":key,"value":curvalue}
 
-
-def getCache(meter,meterdate,curvalue):
-    global lastvaluecache, dayvaluecache
-    last15=-1
-    #last5=-1
-    lastday=-1
-    meterdateold=meterdate- timedelta(minutes=15)
-    meterdateold2=meterdate- timedelta(minutes=5)
-    key=meter+meterdateold.strftime('%Y-%m-%d %H:%M')
-    key2=meter+meterdateold2.strftime('%Y-%m-%d %H:%M')
-
-    if ((meter in lastvaluecache) and (lastvaluecache[meter]["key"]==key)):
-      print('15 minutes')
-      last15=curvalue-lastvaluecache[meter]["value"]
-
-    elif ((meter in lastvaluecache) and (lastvaluecache[meter]["key"]==key2)):
-      print('5 minutes')
-      last15=curvalue-lastvaluecache[meter]["value"]
-    #else:
-    #  print('not compatible : '+ key + '  ' + lastvaluecache[meter]["key"])
-
-
-
-    if((meterdate.hour==0) and (meterdate.minute < 5)):
-      lastday=0
-    else:
-      key=meter+meterdate.strftime('%Y-%m-%d')+" 00:00"
-      if((meter in dayvaluecache) and (dayvaluecache[meter]["key"]==key)):
-        lastday=curvalue-dayvaluecache[meter]["value"]
-
-    #logger.info(lastvaluecache)
-    #logger.info(dayvaluecache)
-
-    return [last15,lastday]
 
 def decodeMetaData(fullpath):
     global site,contract
@@ -315,40 +281,53 @@ def condType3(var1, var2, var3):
     else:
         return 0
 
+def getValueTag(tag, df):
+    if tag in df.index:
+        return df.loc[tag].at[3]
+    else:
+        return 0
+
+
 def cleanData(filename, mes):
     global es
-    logger.info("===> CLEANING DATA <===")
+    #logger.info("===> CLEANING DATA <===")
     if decodeMetaData(filename)==0:
-        logger.info("===> ENTER IF <===")
+        #logger.info("===> ENTER IF <===")
         indexname="OPT_CLEANED_DATA-%s" %(datetime.now().strftime("%Y-%m"))
 
-    dataasio=StringIO(mes)
-    nofsemicolons=mes.count(';')
-    nofcommas=mes.count(',')
+    #dataasio=StringIO(mes)
+    #nofsemicolons=mes.count(';')
+    #nofcommas=mes.count(',')
 
-    if nofsemicolons>nofcommas:
-        df=pd.read_csv(dataasio,sep=";",header=None)
-    else:
-        df=pd.read_csv(dataasio,sep=",",header=None)
+    #if nofsemicolons>nofcommas:
+        #df=pd.read_csv(dataasio,sep=";",header=None)
+    #else:
+    #    df=pd.read_csv(dataasio,sep=",",header=None)
+    
+    df = mes
     
     dfindexed = df.set_index(0)
+    site = 'LUTOSA_ExportNyxAWS'
+    contract = 'COGLTS'
 
     if contract == 'COGLTS':
         dfindexed = df.set_index(0)
-        H2SApThiopaq = dfindexed.loc['LUTOSA_H2S_Ap_Thiopaq'].at[3]
-        H2SAvCogen = dfindexed.loc['LUTOSA_H2S_Av_Cogen'].at[3]
-        H2SAvThiopaq  = dfindexed.loc['LUTOSA_H2S_Av_Thiopaq'].at[3]
 
-        CH4ApThiopaq = dfindexed.loc['LUTOSA_CH4_Ap_Thiopaq'].at[3]
-        CH4AvCogen = dfindexed.loc['LUTOSA_CH4_Av_Cogen'].at[3]
-        CH4AvThiopaq = dfindexed.loc['LUTOSA_CH4_Av_Thiopaq'].at[3]
 
-        O2ApThiopaq = dfindexed.loc['LUTOSA_O2_Ap_Thiopaq'].at[3]
-        O2AvCogen = dfindexed.loc['LUTOSA_O2_Av_Cogen'].at[3]
-        O2AvThiopaq = dfindexed.loc['LUTOSA_O2_Av_Thiopaq'].at[3]
+        H2SApThiopaq = getValueTag('LUTOSA_H2S_Ap_Thiopaq', dfindexed)
+        H2SAvCogen = getValueTag('LUTOSA_H2S_Av_Cogen', dfindexed)
+        H2SAvThiopaq  = getValueTag('LUTOSA_H2S_Av_Thiopaq', dfindexed)
 
-        DebitBiogazThiopaq = dfindexed.loc['COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq'].at[3]
-        DebitBiogazCogen = dfindexed.loc['COGLTS_BIOLTS_Valeur_Debit_Biogaz_Cogen'].at[3]
+        CH4ApThiopaq = getValueTag('LUTOSA_CH4_Ap_Thiopaq', dfindexed)
+        CH4AvCogen = getValueTag('LUTOSA_CH4_Av_Cogen', dfindexed)
+        CH4AvThiopaq = getValueTag('LUTOSA_CH4_Av_Thiopaq', dfindexed)
+
+        O2ApThiopaq = getValueTag('LUTOSA_O2_Ap_Thiopaq', dfindexed)
+        O2AvCogen = getValueTag('LUTOSA_O2_Av_Cogen', dfindexed)
+        O2AvThiopaq = getValueTag('LUTOSA_O2_Av_Thiopaq', dfindexed)
+
+        DebitBiogazThiopaq = getValueTag('COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq', dfindexed)
+        DebitBiogazCogen = getValueTag('COGLTS_BIOLTS_Valeur_Debit_Biogaz_Cogen', dfindexed)
 
         H2SApThiopaq = condType1(H2SApThiopaq, DebitBiogazThiopaq)
         H2SAvCogen = condType3(H2SAvCogen, DebitBiogazCogen, 20)
@@ -378,17 +357,17 @@ def cleanData(filename, mes):
         param1 = dfindexed.at['LUTOSA_Cpt_Ther_HT', 2]
         param2 = dfindexed.at['LUTOSA_Cpt_Ther_HT', 4]
         dffinal = dfindexed.reset_index()
-        fctThiopaqCond1 = 1 if dfindexed.loc['COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq'].at[3] > 120 else 0
-        fctThiopaqCond2 = 1 if (dfindexed.loc['COGLTS_BIOLTS_Valeur_Pression_Thiopaq_Entree'].at[3] > 15 and dfindexed.loc['COGLTS_BIOLTS_Valeur_Pression_Thiopaq_Entree'].at[3] < 36) else 0
-        fctThiopaqCond3 = dfindexed.loc['LUTOSA_Etat_Autor_Biog'].at[3]
+        fctThiopaqCond1 = 1 if getValueTag('COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq', dfindexed) > 120 else 0
+        fctThiopaqCond2 = 1 if (getValueTag('COGLTS_BIOLTS_Valeur_Pression_Thiopaq_Entree', dfindexed) > 15 and getValueTag('COGLTS_BIOLTS_Valeur_Pression_Thiopaq_Entree', dfindexed) < 36) else 0
+        fctThiopaqCond3 = getValueTag('LUTOSA_Etat_Autor_Biog', dfindexed)
         fctThiopaqCondGlobal = 1 if fctThiopaqCond1 + fctThiopaqCond2 + fctThiopaqCond3 == 3 else 0
 
-        consoDispos = dfindexed.loc['LUTOSA_Etat_Peleur1_Fct'].at[3] + dfindexed.loc['LUTOSA_Etat_Peleur2_Fct'].at[3] + dfindexed.loc['LUTOSA_Etat_Blancheur_1'].at[3] +dfindexed.loc['LUTOSA_Etat_Blancheur_2'].at[3]
+        consoDispos = getValueTag('LUTOSA_Etat_Peleur1_Fct', dfindexed) + getValueTag('LUTOSA_Etat_Peleur2_Fct', dfindexed) + getValueTag('LUTOSA_Etat_Blancheur_1', dfindexed) +getValueTag('LUTOSA_Etat_Blancheur_2', dfindexed)
         fctCogenCond1 = 1 if consoDispos >= 2 else 0
-        fctCogenCond2 = 1 if dfindexed.loc['LUTOSA_TempRetour_Boucle_HT'].at[3] <= 80 else 0
-        fctCogenCond3 = 1 if dfindexed.loc['COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq'].at[3] > 220 else 0
-        fctCogenCond4 = dfindexed.loc['LUTOSA_Moteur_Disponible'].at[3]
-        fctCogenCond5 = 1 if dfindexed.loc['LUTOSA_H2S_Av_Cogen'].at[3] <= 5 else 0
+        fctCogenCond2 = 1 if getValueTag('LUTOSA_TempRetour_Boucle_HT', dfindexed) <= 80 else 0
+        fctCogenCond3 = 1 if getValueTag('COGLTS_BIOLTS_Valeur_Debit_Biogaz_Thiopaq', dfindexed) > 220 else 0
+        fctCogenCond4 = getValueTag('LUTOSA_Moteur_Disponible', dfindexed)
+        fctCogenCond5 = 1 if getValueTag('LUTOSA_H2S_Av_Cogen', dfindexed) <= 5 else 0
         fctCogenCondGlobal = 1 if fctCogenCond1 + fctCogenCond2 + fctCogenCond3 + fctCogenCond4 + fctCogenCond5 == 5 else 0
 
 
@@ -415,13 +394,14 @@ def cleanData(filename, mes):
         dffinal['client'] = contract
         dffinal['area'] = site
         dffinal["date"]=dffinal['date'].apply(convertToDate)
-        dffinal["date"]=dffinal['date'].apply(lambda x: utc_to_local(x))
+        #dffinal["date"]=dffinal['date'].apply(lambda x: utc_to_local(x))
         dffinal['@timestamp'] = dffinal['date'].apply(lambda x: getTimestamp(x)*1000)
         dffinal['date'] = dffinal['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:00'))
         dffinal['_index'] = dffinal['date'].apply(lambda x: getIndex(x))
         dffinal['_id'] = dffinal.apply(lambda row: getId(row), axis=1)
 
         es_helper.dataframe_to_elastic(es, dffinal)
+        #return dffinal
 
 
 
@@ -432,88 +412,6 @@ def getId(row):
     _id = ('clean_'+row['client_area_name']+str(row['@timestamp'])).lower()
     return _id
 
-def handleOneMessage(name,body):
-    global es
-    logger.info("===> HANDLE MESSAGE <===")
-    if decodeMetaData(name)==0:
-        logger.info("===> ENTER IF <===")
-        indexname="OPT_SITES_DATA-%s" %(datetime.now().strftime("%Y-%m"))
-
-        #logger.info(lastvaluecache['Conso_Ecl_Ext'])
-        #logger.info(dayvaluecache['Conso_Ecl_Ext'])
-        #logger.info(body)
-
-        dataasio=StringIO(body)
-
-        #logger.info(dataasio)
-        nofsemicolons=body.count(';')
-        nofcommas=body.count(',')
-
-        if nofsemicolons>nofcommas:
-            df=pd.read_csv(dataasio,sep=";",header=None)
-        else:
-            df=pd.read_csv(dataasio,sep=",",header=None)
-
-        #df["date"]=df[1].apply(dateutil.parser.parse)
-        df["date"]=df[1].apply(convertToDate)
-        df=df.dropna()
-
-        bulkbody=""
-
-        #logger.info(df)
-
-        indexname=indexname.lower()
-
-        for index,row in df.iterrows():
-            area_name=site+"_"+row[0]
-            client_area_name=contract+"_"+area_name
-
-            localdate=utc_to_local(row['date'])
-            key=row[0]+localdate.strftime('%Y-%m-%d %H:%M')
-            key2 = row[0]+localdate.strftime('%Y-%m-%d 00:00')
-            print(key)
-            if row[0] in lastvaluecache:
-              if lastvaluecache[row[0]]["key"] != key:
-                cacheres=getCache(row[0],localdate,row[3])
-                ts=localdate.strftime('%s')+"000"
-
-                newrec={"src":"gtc","client_area_name":client_area_name, "type":computeType(row[0]), "area_name":area_name, "client":contract,"area":site,"name":row[0],"@timestamp":ts
-                            ,"date":localdate.strftime('%Y-%m-%d %H:%M:00'),"value":row[3],"value_15":cacheres[0],"value_day":cacheres[1]}
-
-                updateCache(row[0],localdate,row[3])
-                action={}
-                id=(client_area_name+utc_to_local(row['date']).strftime('%Y%m%d%H%M')).lower()
-                action["index"]={"_index":indexname,"_type":"doc","_id":id}
-                bulkbody+=json.dumps(action)+"\r\n"
-                bulkbody+=json.dumps(newrec)+"\r\n"
-              else:
-                logger.info('Duplicate File')
-            else:
-              print("new key")
-              lastvaluecache[row[0]]= {"key": key, "value": 0}
-              dayvaluecache[row[0]]= {"key": key2, "value": 0}
-
-
-            first_alarm_ts = getTimestamp(localdate)
-            obj = {
-                    'start_ts': int(first_alarm_ts),
-                    'area_name': area_name 
-                }
-            #logger.info(obj)     
-        
-        conn.send_message('/topic/SITES_DATA_IMPORTED', json.dumps(obj))
-        
-
-        #logger.info("Final %s=" %(bulkbody))
-        logger.info("Bulk ready.")
-        if(bulkbody != ""):
-            res = es.bulk(body=bulkbody)
-            #logger.info(res)
-        else:
-            logger.error("No BODY !!!")
-        logger.info("Bulk gone.")
-    
-    
       
     
 if __name__ == '__main__':   
@@ -548,14 +446,7 @@ if __name__ == '__main__':
                     ,"login":os.environ["AMQC_LOGIN"],"password":os.environ["AMQC_PASSWORD"]
                     ,"heartbeats":(180000,180000),"earlyack":True}
 
-    lastvaluecache={}
-    dayvaluecache={}
 
-
-
-
-
-  
     logger.info("AMQC_URL          :"+os.environ["AMQC_URL"])
       
       #>> ELK
@@ -569,30 +460,6 @@ if __name__ == '__main__':
         host_params="http://"+os.environ["ELK_URL"]+":"+os.environ["ELK_PORT"]
         es = ES(hosts=[host_params])
 
-    res=es.search(index='opt_*'
-        ,size=0
-        ,body=query
-        )
-    
-    data = res['aggregations']['2']['buckets']
-    
-    for tag in data:
-        tagkey = tag['key']
-        value = tag['1']['hits']['hits'][0]['fields']['value'][0]
-        timestamp = datetime.fromtimestamp(int(tag['1']['hits']['hits'][0]['sort'][0] / 1000))
-        datestr = timestamp.strftime("%Y-%m-%d %H:%M")
-        datestr2 = timestamp.strftime("%Y-%m-%d 00:00")
-        
-        if "fields" in tag['3']['hits']['hits'][0]:
-          value_day = tag['3']['hits']['hits'][0]['fields']['value_day'][0]
-          #print(str(tagkey)+': '+str(value) + ' , ' + str(value_day) + ' ' + datestr)
-          key = tagkey+datestr
-          key2 = tagkey+datestr2
-          lastvaluecache[tagkey]= {"key": key, "value": value}
-          dayvaluecache[tagkey]= {"key": key2, "value": value_day}
-
-    logger.info(lastvaluecache)
-    logger.info(dayvaluecache)
 
     conn=amqstompclient.AMQClient(server
     , {"name":MODULE,"version":VERSION,"lifesign":"/topic/NYX_MODULE_INFO"},QUEUE,callback=messageReceived)
